54d798d29a9a66c420afde91ddc427dd81f3ede2
ArtuSants <138990063+artuSants@users.noreply.github.com>
2023-10-04 12:10:22 -0300

    Teste de mesa 1

---
+++ Desafio 1/Exercicio 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int[] v = new int[6];
+            while (a<6) {
+                v[a] = 10 * a;
+                Console.WriteLine("v[a] = "+ v[a]);
+                a += 1;
+                Console.WriteLine("a = "+ a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Desafio 1/Exercicio 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7, b = a - 6;
+            int[] v = new int[6];
+            Console.WriteLine("b = "+ b);
+            while (b<a)
+            {
+                v[b] = b + a;
+                Console.WriteLine("v[b] = "+ v[b]);
+                b = b + 2;
+                Console.WriteLine("b = " + b);
+            }
+            Console.ReadKey();
+        }
+    }
+}

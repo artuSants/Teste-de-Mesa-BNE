76f4f888522c7d3f7ac0902d412f1e042bedfc68
ArtuSants <138990063+artuSants@users.noreply.github.com>
2023-10-09 14:33:49 -0300

    teste de mesa 2 ex 4

--- Desafio 2/Exercicio 4/Program.cs
+++ Desafio 2/Exercicio 4/Program.cs
@@ -10,7 +10,39 @@ namespace Exercicio_4
     {
         static void Main(string[] args)
         {
-
+            int periodo = 0;
+            Console.WriteLine("Insira o valor a ser aplicado: ");
+            double VP = double.Parse(Console.ReadLine());
+            Console.WriteLine("Isira o periodo em meses: ");
+            double n = double.Parse(Console.ReadLine());
+            Console.WriteLine("Isira o valor da taxa(%): ");
+            double i = double.Parse(Console.ReadLine()) / 100;
+            double RendL, RendA, VF = VP, Saque = 0;
+            for (int j = 0; j < 8; j++)
+            {
+                RendL = (VP * Math.Pow(1 + i, periodo)) - VP;
+                RendA = VP + RendL;
+                VF = VF + RendL;
+                if (periodo == 4)
+                {
+                    Saque += 1000;
+                    VF = RendA + RendL - Saque;
+                    VP = VF;
+                    periodo = 0;
+                }
+                
+                Console.WriteLine("Rendimento Liquido no mes " + (j + 1) + " = " + RendL.ToString("C"));
+                Console.WriteLine("Rendimento Acumulado no mes " + (j + 1) + " = " + RendA.ToString("C"));
+                if(j == 4)
+                {
+                    Console.WriteLine("Saque de " + Saque.ToString("c") + " efetuado!");
+                }
+                Console.WriteLine("Valor Total no mes " + (j + 1) + " = " + VF.ToString("C"));
+                Console.WriteLine("Pressione Enter para continuar!");
+                Console.WriteLine();
+                Console.ReadKey();
+                periodo++;
+            }
         }
     }
 }

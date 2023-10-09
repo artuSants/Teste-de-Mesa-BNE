f8bd534174be10a7b3728d7633f0819e416f26da
ArtuSants <138990063+artuSants@users.noreply.github.com>
2023-10-09 14:55:00 -0300

    Teste de mesa 2 ex 5

---
+++ Desafio 2/Exercicio 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o valor que deseja atingir: ");
+            double VF = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o valor do juros: ");
+            double taxa = double.Parse(Console.ReadLine())/100;
+            Console.WriteLine("Insira o periodo: ");
+            double periodo = double.Parse(Console.ReadLine());
+
+            double VP = VF / Math.Pow(1 + taxa, periodo);
+            Console.WriteLine("Para atingir "+VF.ToString("c")+" Você precisará investir "+VP.ToString("c"));
+            Console.ReadKey();
+        }
+    }
+}
